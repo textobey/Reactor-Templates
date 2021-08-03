4cd2c5a90b049a22c3e380e922500840d40e6696
textObey <dogsan_e@naver.com>
2021-08-03 17:54:13 +0900

    Changed reactor default value

--- reactor.xctemplate/UIViewController/___FILEBASENAME___ViewReactor.swift
+++ reactor.xctemplate/UIViewController/___FILEBASENAME___ViewReactor.swift
@@ -13,30 +13,26 @@ import RxCocoa
 class ___VARIABLE_sceneName___ViewReactor: Reactor {
     /// Action은 사용자와의 상호작용입니다.
     enum Action {
-        case action
+        case emitStep(MainSteps)
     }
     /// Mutation은 뷰에는 노출하지 않고, State를 조작합니다.
     enum Mutation {
-        case mutation(Bool)
+        case emittingStep(MainSteps)
     }
     /// State는 뷰의 현재 상태입니다.
     struct State {
-        var state: Bool
+        var emittedStep:MainSteps = .none
     }
     /// 초기 상태값입니다.
-    let initialState: State
+    let initialState: State = State()
     
-    init() {
-        self.initialState = State(
-            state: false
-        )
-    }
+    init() { }
     
     /// Action -> Mutation
     func mutate(action: Action) -> Observable<Mutation> {
         switch action {
-        case .action:
-            return Observable.just(.mutation(true))
+        case .emitStep(let step):
+            return Observable.just(.emittingStep(step))
         }
     }
     
@@ -44,10 +40,10 @@ class ___VARIABLE_sceneName___ViewReactor: Reactor {
     func reduce(state: State, mutation: Mutation) -> State {
         var state = state
         switch mutation {
-        case .mutation(let set):
-            if set {
-                state.state = true
-            }
+        case .emittingStep(let step):
+            var newState = state
+            newState.emittedStep = step
+            return newState
         }
         return state
     }
